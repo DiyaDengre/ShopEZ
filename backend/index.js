4c5162a0f05a77d2c84db8bd740339cd04420269
DiyaDengre <dengrediya@gmail.com>
2026-03-07 03:26:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,8 @@
-// packages
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors";
-// Utiles
+
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
@@ -18,7 +16,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -34,6 +32,6 @@ app.get("/api/config/paypal", (req, res) => {
 });
 
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "backend", "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));

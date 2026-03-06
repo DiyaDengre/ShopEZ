972d16efe5227cbfc388bb5e27cfee2a985895d4
DiyaDengre <dengrediya@gmail.com>
2026-03-07 03:40:57 +0530

    Update ProductCard.jsx

--- frontend/src/pages/Products/ProductCard.jsx
+++ frontend/src/pages/Products/ProductCard.jsx
@@ -6,73 +6,72 @@ import { toast } from "react-toastify";
 import HeartIcon from "./HeartIcon";
 
 const ProductCard = ({ p }) => {
-  const dispatch = useDispatch();
+const dispatch = useDispatch();
 
-  const addToCartHandler = (product, qty) => {
-    dispatch(addToCart({ ...product, qty }));
-    toast.success("Item added successfully", {
-      position: toast.POSITION.TOP_RIGHT,
-      autoClose: 2000,
-    });
-  };
+const addToCartHandler = (product, qty) => {
+dispatch(addToCart({ ...product, qty }));
+toast.success("Item added successfully", {
+position: toast.POSITION.TOP_RIGHT,
+autoClose: 2000,
+});
+};
 
-  const backendURL = "https://shopez-backend-fzaa.onrender.com";
+const backendURL = "https://shopez-backend-fzaa.onrender.com";
 
-  const imageUrl = `${backendURL}${p?.image}`;
+const imageUrl = p?.image?.startsWith("http")
+? p.image
+: `${backendURL}${p?.image}`;
 
-  return (
-    <div className="max-w-sm relative bg-[#1A1A1A] rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <section className="relative">
-        <Link to={`/product/${p?._id}`}>
-          <span className="absolute bottom-3 right-3 bg-pink-100 text-pink-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-pink-900 dark:text-pink-300">
-            {p?.brand}
-          </span>
+return ( <div className="max-w-sm relative bg-[#1A1A1A] rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"> <section className="relative">
+<Link to={`/product/${p?._id}`}> <span className="absolute bottom-3 right-3 bg-pink-100 text-pink-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-pink-900 dark:text-pink-300">
+{p?.brand} </span>
 
-          <img
-            className="cursor-pointer w-full hover:brightness-75 hover:scale-105 transition duration-300"
-            src={imageUrl}
-            alt={p?.name}
-            style={{ height: "170px", objectFit: "cover" }}
-          />
-        </Link>
+      <img
+        className="cursor-pointer w-full hover:brightness-75 hover:scale-105 transition duration-300"
+        src={imageUrl}
+        alt={p?.name}
+        style={{ height: "170px", objectFit: "cover" }}
+      />
+    </Link>
 
-        <HeartIcon product={p} />
-      </section>
+    <HeartIcon product={p} />
+  </section>
 
-      <div className="p-5">
-        <div className="flex justify-between">
-          <h5 className="mb-2 text-xl text-white">{p?.name}</h5>
+  <div className="p-5">
+    <div className="flex justify-between">
+      <h5 className="mb-2 text-xl text-white">{p?.name}</h5>
 
-          <p className="font-semibold text-pink-500">
-            {p?.price?.toLocaleString("en-US", {
-              style: "currency",
-              currency: "USD",
-            })}
-          </p>
-        </div>
-
-        <p className="mb-3 font-normal text-[#CFCFCF]">
-          {p?.description?.substring(0, 60)} ...
-        </p>
-
-        <section className="flex justify-between items-center">
-          <Link
-            to={`/product/${p?._id}`}
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-pink-700 rounded-lg hover:bg-pink-800"
-          >
-            Read More
-          </Link>
-
-          <button
-            className="p-2 rounded-full"
-            onClick={() => addToCartHandler(p, 1)}
-          >
-            <AiOutlineShoppingCart size={25} />
-          </button>
-        </section>
-      </div>
+      <p className="font-semibold text-pink-500">
+        {p?.price?.toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+        })}
+      </p>
     </div>
-  );
+
+    <p className="mb-3 font-normal text-[#CFCFCF]">
+      {p?.description?.substring(0, 60)} ...
+    </p>
+
+    <section className="flex justify-between items-center">
+      <Link
+        to={`/product/${p?._id}`}
+        className="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-pink-700 rounded-lg hover:bg-pink-800"
+      >
+        Read More
+      </Link>
+
+      <button
+        className="p-2 rounded-full"
+        onClick={() => addToCartHandler(p, 1)}
+      >
+        <AiOutlineShoppingCart size={25} />
+      </button>
+    </section>
+  </div>
+</div>
+
+);
 };
 
 export default ProductCard;

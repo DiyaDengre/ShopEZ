1872b7cf7f2bf46f0a18b4cf8ce12d7ff7f26af0
DiyaDengre <dengrediya@gmail.com>
2026-03-07 03:56:35 +0530

    Refactor ProductCard component for cleaner code

--- frontend/src/pages/Products/ProductCard.jsx
+++ frontend/src/pages/Products/ProductCard.jsx
@@ -16,8 +16,10 @@ const ProductCard = ({ p }) => {
     });
   };
 
+  const imageUrl = "https://shopez-backend-fzaa.onrender.com" + p.image;
+
   return (
-    <div className="max-w-sm relative bg-[#1A1A1A] rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-sm relative bg-[#1A1A1A] rounded-lg shadow">
       <section className="relative">
         <Link to={`/product/${p._id}`}>
           <span className="absolute bottom-3 right-3 bg-pink-100 text-pink-800 text-sm font-medium px-2.5 py-0.5 rounded-full">
@@ -26,7 +28,7 @@ const ProductCard = ({ p }) => {
 
           <img
             className="cursor-pointer w-full hover:brightness-75 hover:scale-105 transition duration-300"
-            src={`https://shopez-backend-fzaa.onrender.com${p.image}`}
+            src={imageUrl}
             alt={p.name}
             style={{ height: "170px", objectFit: "cover" }}
           />
@@ -40,21 +42,18 @@ const ProductCard = ({ p }) => {
           <h5 className="mb-2 text-xl text-white">{p.name}</h5>
 
           <p className="font-semibold text-pink-500">
-            {p.price.toLocaleString("en-US", {
-              style: "currency",
-              currency: "USD",
-            })}
+            ${p.price}
           </p>
         </div>
 
         <p className="mb-3 font-normal text-[#CFCFCF]">
-          {p.description.substring(0, 60)} ...
+          {p.description.substring(0, 60)}...
         </p>
 
         <section className="flex justify-between items-center">
           <Link
             to={`/product/${p._id}`}
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-pink-700 rounded-lg hover:bg-pink-800"
+            className="px-3 py-2 text-sm text-white bg-pink-700 rounded-lg"
           >
             Read More
           </Link>

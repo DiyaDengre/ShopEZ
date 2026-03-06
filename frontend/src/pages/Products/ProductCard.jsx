6c2a90fd7d879e84f2698295141401d107e67a2b
DiyaDengre <dengrediya@gmail.com>
2026-03-07 03:18:14 +0530

    Refactor ProductCard by removing SVG and updating classes
    Removed unnecessary SVG icon from the ProductCard component and simplified the heading class.

--- frontend/src/pages/Products/ProductCard.jsx
+++ frontend/src/pages/Products/ProductCard.jsx
@@ -36,9 +36,7 @@ return ( <div className="max-w-sm relative bg-[#1A1A1A] rounded-lg shadow dark:b
 
   <div className="p-5">
     <div className="flex justify-between">
-      <h5 className="mb-2 text-xl text-white dark:text-white">
-        {p?.name}
-      </h5>
+      <h5 className="mb-2 text-xl text-white">{p?.name}</h5>
 
       <p className="font-semibold text-pink-500">
         {p?.price?.toLocaleString("en-US", {
@@ -55,23 +53,9 @@ return ( <div className="max-w-sm relative bg-[#1A1A1A] rounded-lg shadow dark:b
     <section className="flex justify-between items-center">
       <Link
         to={`/product/${p._id}`}
-        className="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-pink-700 rounded-lg hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800"
+        className="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-pink-700 rounded-lg hover:bg-pink-800"
       >
         Read More
-        <svg
-          className="w-3.5 h-3.5 ml-2"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 10"
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M1 5h12m0 0L9 1m4 4L9 9"
-          />
-        </svg>
       </Link>
 
       <button

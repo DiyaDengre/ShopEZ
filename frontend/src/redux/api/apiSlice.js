e0f238c477f012925316b8343e4a3fe331f57b5c
DiyaDengre <dengrediya@gmail.com>
2026-03-07 13:40:58 +0530

    Update base URL for API requests

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,7 +1,8 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
-import { BASE_URL } from "../constants";
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+const baseQuery = fetchBaseQuery({
+  baseUrl: "https://shopez-backend-fzaa.onrender.com",
+});
 
 export const apiSlice = createApi({
   baseQuery,

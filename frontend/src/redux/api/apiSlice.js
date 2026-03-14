7063fb6ec051836eeaf73cefe203fbec0c0cb483
DiyaDengre <dengrediya@gmail.com>
2026-03-14 21:36:49 +0530

    Add credentials option to base query

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -2,6 +2,7 @@ import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "https://shopez-backend-fzaa.onrender.com",
+  credentials: "include", // ✅ send cookies with every request
 });
 
 export const apiSlice = createApi({
